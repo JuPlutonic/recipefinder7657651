55eada3f98a096c09e87eef2a80fcdb0c887207b
0x639d1dec74 <guhtymailru@mailinator.com>
2016-12-16 22:50:54 +0100

    Typos: formated_time,goodbue,times_displaer. In GreeterController: added tabs, added @name for action #goodbye

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,11 +1,13 @@
 class GreeterController < ApplicationController
 	def hello
-	random_names = ["Alex", "Joe", "Michael"]
-	@name = random_names.sample
-	@time = Time.now
-	@times_displayed ||= 0 #Should work only in gaps of one request
-	@times_displayed += 1
+		random_names = ["Alex", "Joe", "Michael"]
+		@name = random_names.sample
+		@time = Time.now
+		@times_displayed ||= 0 #Should work only in gaps of one request
+		@times_displayed += 1
 	end
-	def goodbue
+	def goodbye
+		random_names = ["Alex", "Joe", "Michael"]
+		@name = random_names.sample
 	end
 end
--- app/helpers/greeter_helper.rb
+++ app/helpers/greeter_helper.rb
@@ -1,5 +1,5 @@
 module GreeterHelper
-	def formated_time(time)
+	def formatted_time(time)
 		# time string in AM/PM format
 		time.strftime("%I:%M%p")
 	end
