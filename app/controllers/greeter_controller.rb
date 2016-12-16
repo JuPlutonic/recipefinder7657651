9dbd901da265688bb7b185561d3134ef050980b1
0x639d1dec74 <guhtymailru@mailinator.com>
2016-12-16 21:34:26 +0100

    moving busines logic out of views to coply MVC (into Greeter_controller.rb

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,5 +1,10 @@
 class GreeterController < ApplicationController
-	def hello
+	def
+	random_names = ["Alex", "Joe", "Michael"]
+	@name = random_names.sample
+	@time = Time.now
+	@times_displayed ||= 0 #Should work only in gaps of one request
+	@times_displayed += 1
 	end
 	def goodbue
 	end
