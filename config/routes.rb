3a522c51326e4fa40aaba2b711e2f0af88e38847
0x639d1dec74 <guhtymailru@mailinator.com>
2016-12-16 23:10:28 +0100

    recipes controller added. pg-prod, sqlite-test,dev added. HTTParty added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+
 	get 'greeter/hello'
 	get 'greeter/goodbye'
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
