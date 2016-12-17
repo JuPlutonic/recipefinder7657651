12b884d1eb4d7dfba1d1dc054cd3488c3e60d8c5
0x639d1dec74 <guhtymailru@mailinator.com>
2016-12-17 14:47:35 +0100

    Comment in recipe.rb changed. Comment added to recipes/index.html.erb Added !___precompiling of assets to spec/spec_helper.rb___!

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,12 +1,6 @@
 class Recipe
 	include HTTParty
 
-	# AWS::S3::Base.establish_connection!(
-	# 	:access_key_id => ENV['S3_KEY'],
-	# 	 :secret_access_key => ENV['S3_SECRET']
-	# 	 :key_value => ENV['FOOD2FORK_KEY']
-	# )
-
 	key_value = ENV["FOOD2FORK_KEY"]
 	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 	base_uri "http://#{hostport}/api"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,20 @@ require 'capybara/dsl'
 require 'capybara/poltergeist'
 
 RSpec.configure do |config|
-  config.include Capybara::DSL
+	config.include Capybara::DSL
+	config.before :all do
+		ENV['PRECOMPILE_ASSETS'] ||= begin
+			case self.class.metadata[:type]
+			when :feature, :view
+				STDOUT.write "Precompiling assets..."
+
+				require 'rake'
+				Rails.application.load_tasks
+				Rake::Task['assets:precompile'].invoke
+
+				STDOUT.puts " done."
+				Time.now.to_s
+			end
+		end
+	end
 end
