70bb9949396f9fc3f78a45ceda1601aea64a6500
0x639d1dec74 <guhtymailru@mailinator.com>
2016-12-17 14:47:51 +0100

    Comment in recipe.rb changed. Comment added to recipes/index.html.erb Added !___precompiling of assets to spec/spec_helper.rb___!

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,9 @@
 class Recipe
 	include HTTParty
 
+	#heroku config:set FOOD2FORK_KEY="c2b5d0ebe1a8da039c568bbf7a2153da"
+
+
 	key_value = ENV["FOOD2FORK_KEY"]
 	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 	base_uri "http://#{hostport}/api"
