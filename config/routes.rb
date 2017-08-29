223a6ab2524c69641a24d0ab1b1665cee55ba513
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-08-30 00:17:34 +0200

    routes.rb: added routes annotation; Gemfile: annotate (gem in development group) set to not to be required.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+# == Route Map
+#
+#          Prefix Verb URI Pattern                Controller#Action
+#   recipes_index GET  /recipes/index(.:format)   recipes#index
+#            root GET  /                          recipes#index
+#
+# for root it is possible to get search in F2F (c) with ?looking_for=
+#
+#   greeter_hello GET  /greeter/hello(.:format)   greeter#hello
+# greeter_goodbye GET  /greeter/goodbye(.:format) greeter#goodbye
+#         contact GET  /contact(.:format)         recipes#contact
+#           about GET  /about(.:format)           recipes#about
+#
+
 Rails.application.routes.draw do
   get 'recipes/index'
   root 'recipes#index'
